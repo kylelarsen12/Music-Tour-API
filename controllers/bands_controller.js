1783d4ef16c2a31ec9858be2e7fa0073175ab987
unknown <kylelarsen12@gmail.com>
2022-09-10 18:49:38 -0400

    Create bands controller, first show routes for bands and individual band

---
+++ controllers/bands_controller.js
@@ -0,0 +1,30 @@
+//DEPENDENCIES
+const bands = require("express").Router();
+const db = require("../models");
+const { Band } = db;
+
+//ROUTES
+//Return all bands
+bands.get("/", async (req, res) => {
+  try {
+    const foundBands = await Band.findAll();
+    res.status(200).json(foundBands);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//return specific band
+bands.get("/:id", async (req, res) => {
+  try {
+    const foundBand = await Band.findOne({
+      where: { band_id: req.params.id },
+    });
+    res.status(200).json(foundBand);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//EXPORT
+module.exports = bands;
