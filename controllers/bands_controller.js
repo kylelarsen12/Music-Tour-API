38d3b67b5e1305e676cb2f83505c2dda643cdeda
unknown <kylelarsen12@gmail.com>
2022-09-10 19:17:12 -0400

    Implement PUT and DELETE routes for bands

--- controllers/bands_controller.js
+++ controllers/bands_controller.js
@@ -40,5 +40,39 @@ bands.post("/", async (req, res) => {
   }
 });
 
+//UPDATE a band
+bands.put("/:id", async (req, res) => {
+  try {
+    const selectedBand = await Band.update(req.body, {
+      where: {
+        band_id: req.params.id,
+      },
+    });
+    res.status(200).json({
+      message: `Succesfully updated band by the band_id of ${selectedBand}`,
+      data: selectedBand,
+    });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//DELETE
+bands.delete("/:id", async (req, res) => {
+  try {
+    const deletedBand = await Band.destroy({
+      where: {
+        band_id: req.params.id,
+      },
+    });
+    res.status(200).json({
+      message: `Successfully deleted the band by the id of ${deletedBand}`,
+      data: deletedBand,
+    });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 //EXPORT
 module.exports = bands;
