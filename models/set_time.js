896c07d4666ec5c5bb7a639abf109850f29f6837
unknown <kylelarsen12@gmail.com>
2022-09-10 20:56:39 -0400

    Add associations to models

--- models/set_time.js
+++ models/set_time.js
@@ -7,8 +7,24 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      // define association here
+    static associate({ Band, Event, Stage }) {
+      //Band
+      Set_Time.belongsTo(Band, {
+        foreignKey: "band_id",
+        as: "band",
+      });
+
+      //Event
+      Set_Time.belongsTo(Event, {
+        foreignKey: "event_id",
+        as: "event",
+      });
+
+      //Stage
+      Set_Time.belongsTo(Stage, {
+        foreignKey: "stage_id",
+        as: "stage",
+      });
     }
   }
   Set_Time.init(
